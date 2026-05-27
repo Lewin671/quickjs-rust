35a083a97de8b66268f6ef2be21d09853ce7b8bb
qingyingliu <qingyingliu671@foxmail.com>
2026-05-27 10:57:02 -0600

    Add lexer comments and punctuators

--- crates/qjs-lexer/src/lib.rs
+++ crates/qjs-lexer/src/lib.rs
@@ -34,10 +34,44 @@ pub enum TokenKind {
     Star,
     /// `/`.
     Slash,
+    /// `%`.
+    Percent,
+    /// `=`.
+    Equal,
+    /// `!`.
+    Bang,
+    /// `<`.
+    Less,
+    /// `>`.
+    Greater,
+    /// `&`.
+    Ampersand,
+    /// `|`.
+    Pipe,
+    /// `^`.
+    Caret,
+    /// `~`.
+    Tilde,
     /// `(`.
     LeftParen,
     /// `)`.
     RightParen,
+    /// `{`.
+    LeftBrace,
+    /// `}`.
+    RightBrace,
+    /// `[`.
+    LeftBracket,
+    /// `]`.
+    RightBracket,
+    /// `,`.
+    Comma,
+    /// `.`.
+    Dot,
+    /// `:`.
+    Colon,
+    /// `?`.
+    Question,
     /// `;`.
     Semicolon,
     /// End of input.
@@ -90,9 +124,26 @@ impl<'src> Lexer<'src> {
                 '+' => self.single(TokenKind::Plus),
                 '-' => self.single(TokenKind::Minus),
                 '*' => self.single(TokenKind::Star),
-                '/' => self.single(TokenKind::Slash),
+                '/' => self.slash_or_comment()?,
+                '%' => self.single(TokenKind::Percent),
+                '=' => self.single(TokenKind::Equal),
+                '!' => self.single(TokenKind::Bang),
+                '<' => self.single(TokenKind::Less),
+                '>' => self.single(TokenKind::Greater),
+                '&' => self.single(TokenKind::Ampersand),
+                '|' => self.single(TokenKind::Pipe),
+                '^' => self.single(TokenKind::Caret),
+                '~' => self.single(TokenKind::Tilde),
                 '(' => self.single(TokenKind::LeftParen),
                 ')' => self.single(TokenKind::RightParen),
+                '{' => self.single(TokenKind::LeftBrace),
+                '}' => self.single(TokenKind::RightBrace),
+                '[' => self.single(TokenKind::LeftBracket),
+                ']' => self.single(TokenKind::RightBracket),
+                ',' => self.single(TokenKind::Comma),
+                '.' => self.single(TokenKind::Dot),
+                ':' => self.single(TokenKind::Colon),
+                '?' => self.single(TokenKind::Question),
                 ';' => self.single(TokenKind::Semicolon),
                 _ => {
                     let start = self.cursor;
@@ -110,6 +161,46 @@ impl<'src> Lexer<'src> {
             span: Span::new(self.cursor, self.cursor),
         });
         Ok(self.tokens)
+    }
+
+    fn slash_or_comment(&mut self) -> Result<(), LexError> {
+        let start = self.cursor;
+        self.advance();
+
+        match self.peek() {
+            Some('/') => {
+                self.advance();
+                while !matches!(self.peek(), None | Some('\n' | '\r')) {
+                    self.advance();
+                }
+                Ok(())
+            }
+            Some('*') => {
+                self.advance();
+                self.block_comment(start)
+            }
+            _ => {
+                self.tokens.push(Token {
+                    kind: TokenKind::Slash,
+                    span: Span::new(start, self.cursor),
+                });
+                Ok(())
+            }
+        }
+    }
+
+    fn block_comment(&mut self, start: usize) -> Result<(), LexError> {
+        while let Some(ch) = self.advance() {
+            if ch == '*' && self.peek() == Some('/') {
+                self.advance();
+                return Ok(());
+            }
+        }
+
+        Err(LexError {
+            message: "unterminated block comment".to_owned(),
+            span: Span::new(start, self.cursor),
+        })
     }
 
     fn identifier(&mut self) {
@@ -200,6 +291,8 @@ fn is_identifier_continue(ch: char) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use qjs_ast::Span;
+
     use super::{TokenKind, lex};
 
     #[test]
@@ -214,6 +307,61 @@ mod tests {
                 TokenKind::Number("42".to_owned()),
                 TokenKind::Semicolon,
                 TokenKind::Eof,
+            ]
+        );
+    }
+
+    #[test]
+    fn skips_line_and_block_comments() {
+        let tokens = lex("one // ignore\n/* skip */ two").expect("source should lex");
+        let kinds: Vec<_> = tokens.into_iter().map(|token| token.kind).collect();
+        assert_eq!(
+            kinds,
+            vec![
+                TokenKind::Identifier("one".to_owned()),
+                TokenKind::Identifier("two".to_owned()),
+                TokenKind::Eof,
+            ]
+        );
+    }
+
+    #[test]
+    fn reports_unterminated_block_comment() {
+        let error = lex("1 /* unfinished").expect_err("comment should fail");
+        assert_eq!(error.message, "unterminated block comment");
+        assert_eq!(error.span, Span::new(2, 15));
+    }
+
+    #[test]
+    fn lexes_common_punctuators_with_spans() {
+        let tokens = lex("{}[](),.:?%=!<>|&^~").expect("source should lex");
+        let actual: Vec<_> = tokens
+            .into_iter()
+            .map(|token| (token.kind, token.span))
+            .collect();
+        assert_eq!(
+            actual,
+            vec![
+                (TokenKind::LeftBrace, Span::new(0, 1)),
+                (TokenKind::RightBrace, Span::new(1, 2)),
+                (TokenKind::LeftBracket, Span::new(2, 3)),
+                (TokenKind::RightBracket, Span::new(3, 4)),
+                (TokenKind::LeftParen, Span::new(4, 5)),
+                (TokenKind::RightParen, Span::new(5, 6)),
+                (TokenKind::Comma, Span::new(6, 7)),
+                (TokenKind::Dot, Span::new(7, 8)),
+                (TokenKind::Colon, Span::new(8, 9)),
+                (TokenKind::Question, Span::new(9, 10)),
+                (TokenKind::Percent, Span::new(10, 11)),
+                (TokenKind::Equal, Span::new(11, 12)),
+                (TokenKind::Bang, Span::new(12, 13)),
+                (TokenKind::Less, Span::new(13, 14)),
+                (TokenKind::Greater, Span::new(14, 15)),
+                (TokenKind::Pipe, Span::new(15, 16)),
+                (TokenKind::Ampersand, Span::new(16, 17)),
+                (TokenKind::Caret, Span::new(17, 18)),
+                (TokenKind::Tilde, Span::new(18, 19)),
+                (TokenKind::Eof, Span::new(19, 19)),
             ]
         );
     }
