0b2704ac6f8e376befac73b1e2135ac713d3202a
qingyingliu <qingyingliu671@foxmail.com>
2026-05-27 11:49:08 -0600

    Add string addition semantics

---
+++ tests/test262/cases/string-number-addition.js
@@ -0,0 +1,5 @@
+// Derived from: test/language/expressions/addition/S11.6.1_A3.2_T2.1.js
+if ("1" + 1 !== "11") { throw; }
+if (1 + "1" !== "11") { throw; }
+if ("x" + 1 !== "x1") { throw; }
+if (1 + "x" !== "1x") { throw; }
