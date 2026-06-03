48a442927ef9ebb9d3a0d8d0cac26d7f65d35cdb
qingyingliu <qingyingliu671@foxmail.com>
2026-06-02 22:33:50 -0600

    Handle function receivers in isPrototypeOf

--- crates/qjs-runtime/src/tests/objects/builtins/prototype_chain.rs
+++ crates/qjs-runtime/src/tests/objects/builtins/prototype_chain.rs
@@ -35,6 +35,16 @@ fn evaluates_object_prototype_chain_checks() {
         Ok(Value::Boolean(false))
     );
     assert_eq!(
+        eval("function F() {} Number.isPrototypeOf(new F());"),
+        Ok(Value::Boolean(false))
+    );
+    assert_eq!(
+        eval(
+            "function User(name) { this.name = name; } function ForcedUser(name, grade) { this.name = name; this.grade = grade; } let proto = new User('noname'); ForcedUser.prototype = proto; let luke = new ForcedUser('Luke Skywalker', 12); proto.isPrototypeOf(luke) && User.prototype.isPrototypeOf(luke) && !Number.isPrototypeOf(luke);"
+        ),
+        Ok(Value::Boolean(true))
+    );
+    assert_eq!(
         eval("Object.prototype.isPrototypeOf(1);"),
         Ok(Value::Boolean(false))
     );
--- tests/test262/cases/object-is-prototype-of-basic.js
+++ tests/test262/cases/object-is-prototype-of-basic.js
@@ -5,3 +5,20 @@ if (typeof Object.prototype.isPrototypeOf !== "function") { throw; }
 if (!proto.isPrototypeOf(object)) { throw; }
 if (!Object.prototype.isPrototypeOf(object)) { throw; }
 if (object.isPrototypeOf(proto)) { throw; }
+
+function USER_FACTORY(name) {
+  this.name = name;
+}
+
+function FORCEDUSER_FACTORY(name, grade) {
+  this.name = name;
+  this.grade = grade;
+}
+
+var userProto = new USER_FACTORY("noname");
+FORCEDUSER_FACTORY.prototype = userProto;
+var luke = new FORCEDUSER_FACTORY("Luke Skywalker", 12);
+
+if (!userProto.isPrototypeOf(luke)) { throw; }
+if (!USER_FACTORY.prototype.isPrototypeOf(luke)) { throw; }
+if (Number.isPrototypeOf(luke)) { throw; }
